e3909c2e7043e80ee5ab2ce232a78e0c63fd6998
vaibhavujagare <vaibhavujagare77@gmail.com>
2020-12-27 15:50:23 +0530

    Add DeafultLayout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,49 @@
 <template>
-  <div>
-    I am layout
-    <Nuxt />
-  </div>
+  <div class="container-fluid">
+    <button class="btn btn-success btn float-right" type="submit" style="margin: 10px 10px;"> Login With Portis</button>         
+    
+        <div class="box">
+          <div class="text">
+            <h1 style="margin: 40px 292px;">Property Explorer</h1> 
+          </div>
+          
+             <input type="search" id="search" placeholder="Search">
+             
+        </div>
+    </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
+
+
+<style scoped>
+.nav-bar{
+  display: flex;
+}
+
+.nav-bar leftNav{
+background-color: aqua;
+width: 50%;
+}
+.rightNav b-button{
+  background-color: black;
+  text-align: right;
+  
+}
+
+.box{
+  font-size: 20px!important;
+    width: 100%;
+    padding: 15.5px 15.5px 15.5px 50px;
+    padding: 270px;
+}
+
+#search {
+  
+    margin: 0 auto;
+    display: grid;
+}
+</style>
